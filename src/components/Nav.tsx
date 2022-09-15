80707ff14a39b513973973f40345b12d4633c8e9
Lu4nScr1pt1ng <rd1qys@gmail.com>
2022-09-15 11:52:16 -0300

    fix: nav links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,30 +1,42 @@
+import Link from "next/link";
+
 export function Nav() {
   return (
     <ul className="flex gap-6 items-center text-textostextos font-nav text-xs lg:text-[16px] lg:gap-12">
       <li>
-        <a className="hover:text-black" href="#home">
-          Clube
-        </a>
+        <Link href="#home">
+          <a className="hover:text-black" href="#home">
+            Clube
+          </a>
+        </Link>
       </li>
       <li>
-        <a className="hover:text-black" href="#benefit">
-          Loja
-        </a>
+        <Link href="/">
+          <a className="hover:text-black" href="#home">
+            Loja
+          </a>
+        </Link>
       </li>
       <li>
-        <a className="hover:text-black" href="#tutorial">
-          Produtores
-        </a>
+        <Link href="#produtores">
+          <a className="hover:text-black" href="#home">
+            Produtores
+          </a>
+        </Link>
       </li>
       <li>
-        <a className="hover:text-black" href="#explore">
-          Ofertas
-        </a>
+        <Link href="#oferta">
+          <a className="hover:text-black" href="#home">
+            Ofertas
+          </a>
+        </Link>
       </li>
       <li>
-        <a className="hover:text-black" href="#testimonial">
-          Eventos
-        </a>
+        <Link href="#events">
+          <a className="hover:text-black" href="#home">
+            Eventos
+          </a>
+        </Link>
       </li>
     </ul>
   );
--- src/components/NavMobile.tsx
+++ src/components/NavMobile.tsx
@@ -1,30 +1,42 @@
+import Link from "next/link";
+
 export function NavMobile() {
   return (
     <ul className="flex flex-col py-5 justify-center items-center text-textostextos gap-y-5">
       <li>
-        <a className="hover:text-black" href="#home">
-          Clube
-        </a>
+        <Link href="#home">
+          <a className="hover:text-black" href="#home">
+            Clube
+          </a>
+        </Link>
       </li>
       <li>
-        <a className="hover:text-black" href="#benefit">
-          Loja
-        </a>
+        <Link href="/">
+          <a className="hover:text-black" href="#home">
+            Loja
+          </a>
+        </Link>
       </li>
       <li>
-        <a className="hover:text-black" href="#tutorial">
-          Produtores
-        </a>
+        <Link href="#produtoes">
+          <a className="hover:text-black" href="#home">
+            Produtores
+          </a>
+        </Link>
       </li>
       <li>
-        <a className="hover:text-black" href="#explore">
-          Ofertas
-        </a>
+        <Link href="#oferta">
+          <a className="hover:text-black" href="#home">
+            Ofertas
+          </a>
+        </Link>
       </li>
       <li>
-        <a className="hover:text-black" href="#testimonial">
-          Eventos
-        </a>
+        <Link href="#events">
+          <a className="hover:text-black" href="#home">
+            Eventos
+          </a>
+        </Link>
       </li>
     </ul>
   );
