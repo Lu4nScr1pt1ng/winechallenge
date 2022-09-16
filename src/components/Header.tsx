ccd9c1daf2508d0dcf078e8c7d3bd36bd1a87999
Lu4nScr1pt1ng <rd1qys@gmail.com>
2022-09-15 21:20:48 -0300

    refactor: beautiful logo in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ import { useCart } from "../context/CartContext";
 import { Nav } from "./Nav";
 import { NavMobile } from "./NavMobile";
 
-import logo from "../../public/images/header/logoreal.png";
+import logo from "../../public/images/header/logo.svg";
 import Image from "next/image";
 
 export function Header() {
